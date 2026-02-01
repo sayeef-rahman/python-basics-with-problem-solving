a838bc575c8e3fcfbf0eefc61747f45fc5ea92fc
sayeef-rahman <sayeef.cse@gmial.com>
2026-02-02 00:04:49 +0100

    feat: float variables

--- variables.py
+++ variables.py
@@ -2,4 +2,8 @@
 
 # 1. Integer variable
 a = 10  # Integer variable
-print("Integer a:", a, type(a))
+print("Integer a:", a, type(a))  # type() function is used to get the data type
+
+# 2. Float variable
+b = 3.14  # Float variable
+print("Float b:", b, type(b))
