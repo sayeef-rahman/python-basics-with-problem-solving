1151523d69750f26f6474b34e182774bac3ed5b8
sayeef-rahman <sayeef.cse@gmial.com>
2026-02-02 00:06:54 +0100

    feat:string variable

--- variables.py
+++ variables.py
@@ -7,3 +7,7 @@ print("Integer a:", a, type(a))  # type() function is used to get the data type
 # 2. Float variable
 b = 3.14  # Float variable
 print("Float b:", b, type(b))
+
+# 3. String variable
+myName = "Sayeef Rahman"  # String variable
+print("String :", myName, type(myName))
