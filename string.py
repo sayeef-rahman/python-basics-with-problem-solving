f8ec5204a9f9f49671e68eb0d805c74ed87b63ba
sayeef-rahman <sayeef.cse@gmial.com>
2026-02-04 22:27:47 +0100

    feat: string strip

--- string.py
+++ string.py
@@ -1,14 +1,17 @@
 # All type of python string operation including some mostly used methods with examples
 
-string = " Hello, World!  \n"  # basic string with whitespace and newline
+message = " Hello, World!  \n"  # basic string with whitespace and newline
 raw_string = r"C:\new\tests"  # raw string (backslashes not escaped)
 
 # multiline with tab. Use the opposite kind of quotes. That is, if your string contains single quotes, use double quotes to wrap the string, and vice versa.
 multi_line = """Line1
 Line2\tTabbed"""
 
-print(string)  # prints including newline
+print(message)  # prints including newline
 print(raw_string)  # prints raw path
 print(multi_line)
 
-print("Length of string:", len(string))  # length (counts all chars)
+print("Length of string:", len(message))  # length (counts all chars)
+print(message.strip())  # strip whitespace from both ends
+print(message.rstrip())  # strip right
+print(message.lstrip())  # strip left
