1f26af255abfaab83c260e6db2a2265e7a7a55e6
sayeef-rahman <sayeef.cse@gmial.com>
2026-02-09 22:39:01 +0100

    feat: basic arithmatic operation comeplete

---
+++ numbersMathematicalOperations.py
@@ -0,0 +1,10 @@
+numberOne = 10
+numberTwo = 5
+
+print(f"Addition: {numberOne} + {numberTwo} = {numberOne + numberTwo}")
+print(f"Subtraction: {numberOne} - {numberTwo} = {numberOne - numberTwo}")
+print(f"Multiplication: {numberOne} * {numberTwo} = {numberOne * numberTwo}")
+print(f"Division: {numberOne} / {numberTwo} = {numberOne / numberTwo}")
+print(f"Floor Division: {numberOne} // {numberTwo} = {numberOne // numberTwo}")
+print(f"Modulus (Remainder): {numberOne} % {numberTwo} = {numberOne % numberTwo}")
+print(f"Exponentiation: {numberOne} ** {numberTwo} = {numberOne ** numberTwo}")
