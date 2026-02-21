9134b68fcd00ef2be3c92d509310a971d842e5f6
sayeef-rahman <sayeef.cse@gmial.com>
2026-02-21 17:02:30 +0100

    feat: unpacking & nexted examples

--- tuples.py
+++ tuples.py
@@ -10,3 +10,26 @@ print(empty_tuple)  # Output: ()
 print(programming_languages)  # Output: ('Python', 'Java', 'C++')
 print(number_tuple)  # Output: (1, 2, 3, 4, 5)
 print(mixed_tuple)  # Output: (1, 'hello', 3.14, True)
+print(
+    f"Length of programming_languages tuple: {len(programming_languages)}"
+)  # Output: 3
+
+developer = "Jessica"
+tuple(developer)  # ('J', 'e', 's', 's', 'i', 'c', 'a')
+
+# finding item in tuple
+print("Python" in programming_languages)  # Output: True
+print("Ruby" in programming_languages)  # Output: False
+
+
+# unpacking tuples
+a, b, c = programming_languages
+print(a)  # Output: Python
+print(b)  # Output: Java
+print(c)  # Output: C++
+
+
+# nested tuples
+nested_tuple = (1, (2, 3), (4, 5, 6))
+print(nested_tuple)  # Output: (1, (2, 3), (4, 5, 6))
+print(nested_tuple[0])  # Output: 1
