71b4f8de155b4d2bcf91535f9c3f7c04796555f0
sayeef-rahman <sayeef.cse@gmial.com>
2026-02-21 12:53:59 +0100

    feat: list concatenation

--- list.py
+++ list.py
@@ -7,3 +7,12 @@ fruits_list = ["banana", "guava", "mango", "papaya"]
 print(fruits_list)
 print(fruits_list[0])  # Accessing first element
 print(fruits_list[-1])  # Accessing last element
+
+# string to list
+letters_in_banana = list(fruits_list[0])
+print(f"letters_in_banana: {letters_in_banana}")
+
+# List concatenation
+more_fruits = ["orange", "kiwi"]
+all_fruits = fruits_list + more_fruits
+print(f"All fruits: {all_fruits}")
