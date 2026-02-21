c2d5a91790b7dcf2eb15f4d680eed6fdaaa11677
sayeef-rahman <sayeef.cse@gmial.com>
2026-02-21 12:50:58 +0100

    feat: non empty list with fixed indexing

--- list.py
+++ list.py
@@ -2,3 +2,8 @@ empty_list = []
 print(empty_list)
 print(type(empty_list))
 print(len(empty_list))
+
+fruits_list = ["banana", "guava", "mango", "papaya"]
+print(fruits_list)
+print(fruits_list[0])  # Accessing first element
+print(fruits_list[-1])  # Accessing last element
