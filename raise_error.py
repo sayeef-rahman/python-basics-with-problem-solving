fccdacc4daeca1812349b775f22bb5b7b4a2d63d
sayeef-rahman <sayeef.cse@gmial.com>
2026-03-18 21:41:28 +0100

    feat: try except error handling

--- raise_error.py
+++ raise_error.py
@@ -7,4 +7,18 @@ def check_adult(age):
     return "You are an adult."
 
 
-check_adult(10)
+print(check_adult(10))
+
+# raise with try
+
+
+def check_age(age):
+    if age < 0:
+        raise ValueError("Age can not be negative!")
+    return age
+
+
+try:
+    check_age(-1)
+except ValueError as e:
+    print(f"Error: {e}")
